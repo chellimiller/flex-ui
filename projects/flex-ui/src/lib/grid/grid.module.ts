43882386ae2987fe4f281a7da72fc67efd77c920
Michelle <chellicmiller@gmail.com>
2018-10-15 20:48:38 -0500

    #1 Generated grid module

---
+++ projects/flex-ui/src/lib/grid/grid.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+/**
+ * Grid Module
+ *
+ * Provides structure for layouts on webpage.
+ */
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: []
+})
+export class GridModule { }
